33c34a8c7c4c6a8561c01cd7eff2255d0c5f823b
Marc Zalik <marc.zalik@gmail.com>
2023-06-13 20:20:15 -0400

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+-- Better escape using jk in insert and terminal mode
+keymap("i", "jk", "<ESC>", default_opts)
+keymap("t", "jk", "<C-\\><C-n>", default_opts)
+
+-- Paste over currently selected text without yanking it
+keymap("v", "p", '"_dP', default_opts)
+
+-- Toggleterm
+keymap("n", "<C-\\>", "<cmd>ToggleTerm<cr>", default_opts)
+
+-- Resizing panes
+keymap("n", "<Left>", ":vertical resize +1<CR>", default_opts)
+keymap("n", "<Right>", ":vertical resize -1<CR>", default_opts)
+keymap("n", "<Up>", ":resize -1<CR>", default_opts)
+keymap("n", "<Down>", ":resize +1<CR>", default_opts)
---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    {
+      "<leader>a",
+      "<cmd>AerialToggle!<CR>",
+      desc = "Aerial Toggle",
+    },
+  },
+}
